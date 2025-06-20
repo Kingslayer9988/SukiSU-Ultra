3ad9117b25792cac947ac19465506baaf78639ac
Kingslayer9988 <king.slayer@tuta.io>
2025-06-20 11:14:15 +0000

    https://github.com/backslashxx/KernelSU/commit/bbeeb36.patch

--- kernel/ksud.c
+++ kernel/ksud.c
@@ -632,14 +632,32 @@ int __maybe_unused ksu_handle_execve_ksud(const char __user *filename_user,
 	return ksu_common_execve_ksud(filename_user, &argv);
 }
 
-#if defined(CONFIG_COMPAT) && defined(CONFIG_64BIT)
-int __maybe_unused ksu_handle_compat_execve_ksud(const char __user *filename_user,
+#if defined(CONFIG_64BIT) && defined(CONFIG_COMPAT)
+__maybe_unused int ksu_handle_compat_execve_ksud(const char __user *filename_user,
 			const compat_uptr_t __user *__argv)
 {
 	struct user_arg_ptr argv = { .ptr.compat = __argv };
-	return ksu_common_execve_ksud(filename_user, &argv);
+	struct filename filename_in, *filename_p;
+	char path[32];
+
+	// return early if disabled.
+	if (!ksu_execveat_hook) {
+		return 0;
+	}
+
+	if (!filename_user)
+		return 0;
+
+	memset(path, 0, sizeof(path));
+	ksu_strncpy_from_user_nofault(path, filename_user, 32);
+
+	// this is because ksu_handle_execveat_ksud calls it filename->name
+	filename_in.name = path;
+	filename_p = &filename_in;
+    
+	return ksu_handle_execveat_ksud(AT_FDCWD, &filename_p, &argv, NULL, NULL);
 }
-#endif /* COMPAT & 64BIT */
+#endif
 
 #endif
 
